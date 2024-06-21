7acff75e9fe08c2e58e0a4c6ed6b4f585d0687f7
lee mighdoll <lee@underneath.ca>
2024-06-21 11:32:07 -0700

    update description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "Mighdoll Blog";
-export const SITE_DESCRIPTION = "Programming Blog: WebGPU, TypeScript, Parsing";
+export const SITE_DESCRIPTION = "Programmer's Blog: Data Visualization, WebGPU, TypeScript, Parsing";
